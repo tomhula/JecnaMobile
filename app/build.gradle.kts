676f4a240c77ea03f9482d3a355133e787049ba6
Tomas Hula <tomashula06@gmail.com>
2025-09-23 22:05:28 +0200

    Bump version to 2.4.4 code 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "me.tomasan7.jecnamobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "2.4.3"
+        versionCode = 30
+        versionName = "2.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
