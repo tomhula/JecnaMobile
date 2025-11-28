556577f2b91544cbd6552e970119c5b0d2d867da
Tomas Hula <tomashula06@gmail.com>
2025-11-28 21:56:46 +0100

    Bump version to 2.5.2 code 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "me.tomasan7.jecnamobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "2.5.1"
+        versionCode = 34
+        versionName = "2.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
