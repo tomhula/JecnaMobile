1c8d9551caea5939ad0aac2c22656a4856f99076
Tomas Hula <tomashula06@gmail.com>
2025-12-12 16:08:40 +0100

    Put back enableEdgeToEdge()

--- app/src/main/kotlin/me/tomasan7/jecnamobile/MainActivity.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : ComponentActivity()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         val startDestination = if (authRepository.exists())
