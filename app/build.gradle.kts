aef8f6155b4fce48ba751587f488c20a4a9bbf5a
Tomas Hula <tomashula06@gmail.com>
2025-12-12 12:21:38 +0100

    Configure debug version to be seperate from release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ android {
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-SNAPSHOT"
+        }
     }
 
     compileOptions {
