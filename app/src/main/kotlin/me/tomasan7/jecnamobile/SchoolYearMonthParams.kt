924a077703351e7cdd39e94e382cf63fda0c4703
Tomas Hula <tomashula06@gmail.com>
2026-01-21 14:43:10 +0100

    Move SchoolYearMonthParams and NoParams serializers together

--- app/src/main/kotlin/me/tomasan7/jecnamobile/SchoolYearMonthParams.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/SchoolYearMonthParams.kt
@@ -2,10 +2,34 @@ package me.tomasan7.jecnamobile
 
 import io.github.tomhula.jecnaapi.util.SchoolYear
 import kotlinx.datetime.Month
+import kotlinx.datetime.number
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable(with = SchoolYearMonthParamsSerializer::class)
 data class SchoolYearMonthParams(
     val schoolYear: SchoolYear,
     val month: Month
 )
+
+internal object SchoolYearMonthParamsSerializer : KSerializer<SchoolYearMonthParams>
+{
+    override val descriptor = PrimitiveSerialDescriptor("SchoolYearMonthParams", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: SchoolYearMonthParams) = encoder.encodeString(
+        "${value.schoolYear.firstCalendarYear}$DIVIDER${value.month.number}"
+    )
+
+    override fun deserialize(decoder: Decoder): SchoolYearMonthParams
+    {
+        val string = decoder.decodeString()
+        val split = string.split(DIVIDER, limit = 2)
+        return SchoolYearMonthParams(SchoolYear(split[0].toInt()), Month(split[1].toInt()))
+    }
+
+    private const val DIVIDER = "|"
+}
