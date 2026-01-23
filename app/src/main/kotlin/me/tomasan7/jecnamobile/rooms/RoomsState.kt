ff8b925d9aa3c3d3f20701c4574b76debdfd9677
Stevek <vegh@spsejecna.cz>
2026-01-23 15:42:46 +0100

    remove deprecated locale

--- app/src/main/kotlin/me/tomasan7/jecnamobile/rooms/RoomsState.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/rooms/RoomsState.kt
@@ -1,11 +1,11 @@
 package me.tomasan7.jecnamobile.rooms
 
-import android.icu.text.Collator
 import androidx.compose.runtime.Immutable
 import de.palm.composestateevents.StateEventWithContent
 import de.palm.composestateevents.consumed
 import io.github.tomhula.jecnaapi.data.room.RoomsPage
 import me.tomasan7.jecnamobile.util.removeAccent
+import java.text.Collator
 import java.util.*
 
 @Immutable
@@ -17,9 +17,8 @@ data class RoomsState(
 )
 {
     val RoomReferencesSorted = roomsPage?.roomReferences
-        ?.sortedWith(compareBy(Collator.getInstance(Locale("cs"))) { it.name })
+        ?.sortedWith(compareBy(Collator.getInstance(Locale.forLanguageTag("cs"))) { it.name })
 
     val RoomReferencesSortedFiltered = RoomReferencesSorted
         ?.filter { it.name.removeAccent().contains(filterFieldValue.removeAccent(), ignoreCase = true) || it.roomCode.contains(filterFieldValue, ignoreCase = true) }
 }
-
