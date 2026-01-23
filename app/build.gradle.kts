51e7d67928fda8c48d98bad9eb35674c20dcd626
Stevek <vegh@spsejecna.cz>
2026-01-23 16:46:52 +0100

    Delete unused lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,8 +82,6 @@ dependencies {
     implementation(libs.accompanist.permissions)
     implementation(libs.navigation3.ui)
     implementation(libs.navigation3.runtime)
-    implementation(libs.composeDestinations.core)
-    ksp(libs.composeDestinations.ksp)
 
     implementation(libs.activity.ktx)
 
