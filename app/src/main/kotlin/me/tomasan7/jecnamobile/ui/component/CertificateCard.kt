2cbbe8613b5c72bb68774ac22c6e0026aa6518a5
jzitnik-dev <email@jzitnik.dev>
2026-03-22 09:32:28 +0100

    perf: Use remember for date formatting

--- app/src/main/kotlin/me/tomasan7/jecnamobile/ui/component/CertificateCard.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/ui/component/CertificateCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -25,6 +26,10 @@ fun CertificateCard(
     certificate: Certificate,
     modifier: Modifier = Modifier
 ) {
+    val formattedDate = remember(certificate.dateIssued) {
+        certificate.dateIssued.format(DATE_FORMATTER)
+    }
+    
     Surface(
         tonalElevation = 4.dp,
         modifier = modifier,
@@ -57,7 +62,7 @@ fun CertificateCard(
                     tint = jm_label
                 )
                 Text(
-                    text = certificate.dateIssued.format(DATE_FORMATTER),
+                    text = formattedDate,
                     style = MaterialTheme.typography.labelMedium,
                     color = jm_label
                 )
